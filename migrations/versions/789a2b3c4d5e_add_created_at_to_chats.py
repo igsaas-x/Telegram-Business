e2c39f6fc7bb53e709b418e9aa8f865677e1ed29
chanhengseang <chanheng.seang@mbanq.com>
2025-07-08 21:36:37 -0700

    fix migration

--- migrations/versions/789a2b3c4d5e_add_created_at_to_chats.py
+++ migrations/versions/789a2b3c4d5e_add_created_at_to_chats.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "789a2b3c4d5e"
-down_revision: Union[str, Sequence[str], None] = "681f7a4dbcf4"
+down_revision: Union[str, Sequence[str], None] = "03_modify_name_fields"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
