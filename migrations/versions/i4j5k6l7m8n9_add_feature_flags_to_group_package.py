a376df077e29e4298b4a8718d5b3c3f22eea95df
chanhengseang <chanheng.seang@mbanq.com>
2025-08-02 00:19:26 -0700

    feature flag

---
+++ migrations/versions/i4j5k6l7m8n9_add_feature_flags_to_group_package.py
@@ -0,0 +1,28 @@
+"""add feature_flags to group_package
+
+Revision ID: i4j5k6l7m8n9
+Revises: 48a5b6c7d8e9
+Create Date: 2025-02-02 10:00:00.000000+07:00
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = 'i4j5k6l7m8n9'
+down_revision: Union[str, Sequence[str], None] = '48a5b6c7d8e9'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    # Add feature_flags column as JSON to store key-value pairs for additional features
+    op.add_column('group_package', sa.Column('feature_flags', sa.JSON(), nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column('group_package', 'feature_flags')
